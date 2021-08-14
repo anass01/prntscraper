b75e0460078cad92e4ec67af0e3fc0cb3d86db4b
Anass Bouallaga <anass0bouallaga0@gmail.com>
2021-08-14 12:28:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
